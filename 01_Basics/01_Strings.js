3890d06c79d65fa2b418774c3f5bad902af4112f
Onkar Karbhal <84263421+OnkarKarbhal@users.noreply.github.com>
2023-11-10 18:31:56 +0000

    strings

---
+++ 01_Basics/01_Strings.js
@@ -0,0 +1,13 @@
+let name  = "Virat"
+let name2 = " Kohli"
+
+//console.log(name+ name2);
+
+let player01 = new String(name+name2)
+
+
+//console.log(player01[0]);
+console.log(player01.charAt(5));
+console.log(player01.toUpperCase());
+
+
